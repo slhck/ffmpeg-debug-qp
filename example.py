e9aa288ef769a06083305dbe882e36e70fec1a4f
nathantrevivian <nathan@cameraforensics.com>
2020-03-02 06:00:27 -0500

    Output only averages if people want to keep the output to a minimum and are only itereste in averages

--- example.py
+++ example.py
@@ -7,12 +7,15 @@ def main():
     parser.add_argument("-o", "--output", help="Output file (defaults to LD-JSON)")
     parser.add_argument("-f", "--force", action="store_true", help="Overwrite output")
     parser.add_argument("-of", "--output-format", default="ld-json", help="Output format, one of: ld-json (default), json or csv")
-    parser.add_argument("-p", "--path-to-tool", required=False, default="./", help="Path to ffmpeg-debug-qp")
-    parser.add_argument("-m", "--include-macroblock-data", action="store_true", help="Include macroblock-level data")
+    parser.add_argument("-p", "--path-to-tool", required=False, default="/usr/local/bin/", help="Path to ffmpeg-debug-qp (defaults to /usr/local/bin/)")
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-m", "--include-macroblock-data", action="store_true", help="Include macroblock-level data, such as: type, interlaced and segmentation")
+    group.add_argument("-a", "--compute-averages-only", action="store_true", help="Only compute the frame-average QPs")
 
     args = vars(parser.parse_args())
     parse_qp_output.set_path(args["path_to_tool"])
-    if parse_qp_output.extract_qp_data(args["video"], args["output"], macroblock_data=args["include_macroblock_data"], force=args["force"], output_format=args["output_format"]):
+    if parse_qp_output.extract_qp_data(args["video"], args["output"], compute_averages_only=args["compute_averages_only"], macroblock_data=args["include_macroblock_data"], force=args["force"], output_format=args["output_format"]):
         print("Success!")
     
 if __name__ == '__main__':
