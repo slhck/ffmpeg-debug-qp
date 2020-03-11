a69d69806f3bb87bdc9c25cb581a9b564c956b2f
Lars The <lars18th@users.noreply.github.com>
2020-03-11 12:12:15 +0100

    fixes frame_type printing
    Fixes the bug "frame_type:" message in the middle of a line.

--- ffmpeg_debug_qp.c
+++ ffmpeg_debug_qp.c
@@ -95,7 +95,8 @@ static int decode_packet(int *got_frame, int cached)
     }
 
    if (*got_frame) {
-        fprintf(stderr,"frame_type: %c; pkt_size: %d\n",
+       fflush(stderr);
+       fprintf(stderr,"<frame_type: %c; pkt_size: >%d\n",
                 av_get_picture_type_char(frame->pict_type),
                 av_frame_get_pkt_size(frame));
     }
