f2d4c5284b89c28255fc53bf25e82aabf59c43b0
Werner Robitza <werner.robitza@gmail.com>
2022-12-11 17:28:02 +0100

    add new python versions

--- setup.py
+++ setup.py
@@ -31,12 +31,12 @@ setup(
         "Topic :: Multimedia :: Video",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     packages=["ffmpeg_debug_qp_parser"],
     entry_points={
         "console_scripts": [
